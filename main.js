4e0411ed51f8641e2dd74a27c53a4e8d7cb7c21d
cally-yt <93189620+callyyt@users.noreply.github.com>
2023-05-02 18:45:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
+
+noseX=0;
+noseY=0;
 function preload()
-{}
+{
+    mustach = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
 
 function setup()
 {
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(300,250);
     canvas.center();
     video= createCapture(VIDEO);
-    video.size(300,300);
+    video.size(300,290);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -20,6 +26,7 @@ function modelLoaded()
 function draw()
 {
     image(video, 0 , 0 ,300, 300);
+    image(mustach, noseX, noseY, 30, 30);
 }
 
 function take_snapshot()
@@ -32,7 +39,9 @@ function gotPoses(results)
     if(results.length >0)
     {
         console.log(results);
-        console.log("nose x=" + results[0].pose.nose.x);
-        console.log("nose y=" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-14;
+        noseY = results[0].pose.nose.y-4;
+        console.log("nose x=" + noseX);
+        console.log("nose y=" + noseY);
     }
 }
